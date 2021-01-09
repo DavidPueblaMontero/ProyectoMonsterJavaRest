a42a6e1e93a97318a8743322c33e76293d45f723
David Alexander Puebla Montero <20991118+DavidPueblaMontero@users.noreply.github.com>
2021-01-09 11:32:24 -0500

    s

---
+++ 01 Servers/01_Server_CoreBancario/src/java/ec/edu/monster/controller/DBConnect.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.monster.controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author john, jess, david
+ */
+public class DBConnect {
+
+    Connection ct;
+    String bd = "corebankmysql";
+    String url = "jdbc:mysql://localhost/" + bd;
+    String user = "root";
+    String pass = "11023650";
+
+    public Connection connect() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            ct = (Connection) DriverManager.getConnection(url, user, pass);
+            System.out.println("Successful connection");
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("ERROR");
+        }
+        return ct;
+    }
+
+    public boolean confirmConnect() {
+        boolean confirm;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            ct = (Connection) DriverManager.getConnection(url, user, pass);
+            confirm = true;
+        } catch (ClassNotFoundException | SQLException ex) {
+            confirm = false;
+        }
+        return confirm;
+    }
+
+
+    public static void main(String[] args) throws SQLException {
+        DBConnect c = new DBConnect();
+        c.connect();
+        
+    }
+
+}
